092dbe5378c66fd068e4c8c1839272345706e664
Nikita <nperveytalov7@gmail.com>
2025-01-11 20:00:34 +0300

    Delete Namespace.py

--- Namespace.py
+++
@@ -1,9 +0,0 @@
-def test_function():
-    def inner_function(*args):
-        a = "Я в области видимости функции test_function"
-        return a
-    inner_function()
-    a = inner_function()
-    print(inner_function())
-
-test_function()
